bfc1cae8542fe6952fc3660d1b588b1e444b0b2e
KevinChauke <s.chauke@lab.clicklearning.org>
2023-11-28 14:38:15 +0200

    editted css

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Season from "../components/Season.jsx";
 import SeasonDisplay from "../components/SeasonDisplay.jsx";
 const SHOW_API = "https://podcast-api.netlify.app/id/";
